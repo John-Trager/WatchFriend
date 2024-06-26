2ca618ed4d4b096ded4e9399f91ab5dfd173b8ac
John-Trager <johntrager0226@gmail.com>
2024-06-26 15:31:17 -0400

    Initial Commit

---
+++ Friend Watch App/FriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendApp.swift
+//  Friend Watch App
+//
+//  Created by John Trager on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Friend_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
